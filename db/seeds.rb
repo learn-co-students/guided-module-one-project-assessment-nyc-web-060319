cf3e64695833babd532f6c53d2ae072d770375fa
Alexander Riccio <test35965+learn@gmail.com>
2019-06-18 13:53:04 -0400

    seeds messing around, sorry master

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-if ENV["CHATBOT_ENV"] != "test"
-    puts "Will not seed the production/development database!!!"
-    raise StandardError, "No."
-end
+# if ENV["CHATBOT_ENV"] != "test"
+#     puts "Will not seed the production/development database!!!"
+#     raise StandardError, "No."
+# end
 
